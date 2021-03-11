44f1ca989a9cfddac4f3f8a5b61a6aaf81fa4063
hohaidang <hohaidang.khtn@gmail.com>
2021-03-11 21:55:30 +0900

    re-format

--- 003_GPIO_driver_CPP/Src/003_led_button.cpp
+++ 003_GPIO_driver_CPP/Src/003_led_button.cpp
@@ -5,31 +5,29 @@
  *      Author: prnsoft
  */
 
-
-
 #include "../driver/stm32f446re_gpio_driver.h"
 
-
 void delay(void) {
-	for(uint32_t i = 0; i < 500000; ++i){};
+  for (uint32_t i = 0; i < 500000; ++i) {
+  };
 }
 
 void small_delay(void) {
-	for(uint32_t i = 0; i < 250000; ++i){};
+  for (uint32_t i = 0; i < 250000; ++i) {
+  };
 }
 
-int main(void)
-{
-    gpio_handler LED2(GPIOA);
-    LED2.gpio_init(GPIO_PIN_NO_5, GPIO_MODE_OUT, GPIO_SPEED_LOW, GPIO_OP_TYPE_PP, GPIO_NO_PUPD);
-    gpio_handler user_button(GPIOC);
-    user_button.gpio_init(GPIO_PIN_NO_13, GPIO_MODE_IN, GPIO_SPEED_LOW);
-	LED2.gpio_write_to_output_pin(1);
-	for(;;) {
-		if(!user_button.gpio_read_from_input_pin()) {
-			small_delay(); // remove debouncing for Botton
-			LED2.gpio_toggle_output_pin();
-		}
-	}
-
+int main(void) {
+  gpio_handler LED2(GPIOA);
+  LED2.gpio_init(GPIO_PIN_NO_5, GPIO_MODE_OUT, GPIO_SPEED_LOW, GPIO_OP_TYPE_PP,
+                 GPIO_NO_PUPD);
+  gpio_handler user_button(GPIOC);
+  user_button.gpio_init(GPIO_PIN_NO_13, GPIO_MODE_IN, GPIO_SPEED_LOW);
+  LED2.gpio_write_to_output_pin(1);
+  for (;;) {
+    if (!user_button.gpio_read_from_input_pin()) {
+      small_delay(); // remove debouncing for Botton
+      LED2.gpio_toggle_output_pin();
+    }
+  }
 }
